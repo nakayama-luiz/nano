9ad640d95bf011dd1f57d4ee7b491fbb927e0840
provavelmente <110038923+nakayama-luiz@users.noreply.github.com>
2023-08-24 22:06:32 -0300

    user crud complete
    external validation

--- user.py
+++ user.py
@@ -25,3 +25,23 @@ def view_user(id):
     conn.commit()
     conn.close()
     return usuario
+
+
+
+def update_user_name(id, nome):
+  conn = sqlite3.connect('database.db')
+  cursor = conn.cursor()
+  cursor.execute(f"update perfil set nome = '{nome}' where id = {id}")
+  conn.commit()
+  conn.close()
+  print(f'teve seu usuário atualizado para: {nome}')
+
+
+def get_id_by_name(name):
+  conn = sqlite3.connect('database.db')
+  cursor = conn.cursor()
+  cursor.execute(f"select id from perfil where nome = '{name}' ")
+  usuario = cursor.fetchall()
+  conn.commit()
+  conn.close()
+  return usuario[0][0]
